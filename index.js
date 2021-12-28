6a6fb83c0957d878c1ef7b5c4c0e6ace3557c1c5
PiHyoJeong <shson4007@gmail.com>
2021-12-28 18:10:26 +0900

    connect mongodb and create mongoose schema

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express()
 const port = 8080
+//mongoose를 이용하여 어플리케이션과 mongo db 연결
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://HyoJeong:abcde1234@cluster0.qzcc8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    usenewurlparser: true, useUnifiedTopology: true
+}).then(()=>console.log('MongoDb Connected..'))
+  .catch(err=>console.log(err))
 
 app.get('/',(req,res)=> res.send('Hello Hyojeong!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose'); //mongoose module 가져오기
+
+const userSchema = mongoose.Schema({ //mongoose를 이용하여 schema 생성
+    name:{
+        type: String,
+        maxlength: 50
+    },
+    email:{
+        type: String,
+        trim: true, //빈칸 제거
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token:{
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema) //model(model이름,schema)
+
+module.exports = { User } //model 을 다른 파일에서도 쓰기 위해 export하기
