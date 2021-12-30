1a30016cc39a695561933ca5302ccce49ac95d2e
PiHyoJeong <shson4007@gmail.com>
2021-12-30 23:58:02 +0900

    encryption  using bcrypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose'); //mongoose module 가져오기
-
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 const userSchema = mongoose.Schema({ //mongoose를 이용하여 schema 생성
     name:{
         type: String,
@@ -25,6 +26,24 @@ const userSchema = mongoose.Schema({ //mongoose를 이용하여 schema 생성
     tokenExp: {
         type: Number
     }
+})
+
+userSchema.pre('save',function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+        //비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds,function(err,salt){
+            if(err) return next(err)
+            bcrypt.hash(user.password,salt,function(err,hash){
+                if(err) return next(err)
+                user.password=hash
+                next()
+            })
+        })
+    }
+    
+    
 })
 
 const User = mongoose.model('User', userSchema) //model(model이름,schema)
